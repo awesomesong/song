fdba9534c7fe9183e61715289045c8a016e68fe6
songhee <kshs24820@gmail.com>
2019-09-16 14:37:27 +0900

    complete quiz

--- quiz/js/quiz.js
+++ quiz/js/quiz.js
@@ -1,19 +1,90 @@
-// 문제의 답
-var q_answer = ["2"];
+    //javascript 
+    document.addEventListener("DOMContentLoaded", function() { 
+        var quiz_check = document.querySelectorAll('.quiz_answer_list_txt');
+            for(var i =0; i < quiz_check.length; i++) {
+                quiz_check[i].onclick = function(){
+                    var a = this.previousElementSibling.value;         
+                        step1 = document.querySelector(".quiz_step_img"),
+                        step2 = document.querySelector(".quiz_step_txt"),
+                        answerO = document.querySelector(".quiz_result_O"),
+                        answerX = document.querySelector(".quiz_result_X");
 
-    //var a = document.querySelector(".quiz_answer_radio");
-    
-   $(document).ready(function(){
+                    if(a == "1" || "2" || "3" || "4"){
+                        step1.classList.remove("hidden");
+                        step2.classList.remove("active");
+                        answerO.classList.remove("active");
+                        answerX.classList.remove("active");
+                    };
 
-        $(".quiz_answer_list_txt").click(function(){
+                    var result = document.querySelector(".quiz_step_img");
+                    result.onclick = function(){
+                        if(q_answer[0] == a){
+                            answerO.classList.add("active");
+                            answerX.classList.remove("active");
+                        }else{
+                            answerO.classList.remove("active");
+                            answerX.classList.add("active");
+                        }
+                    
+                    step1.classList.add("hidden");
+                    step2.classList.add("active");
+                    };
+                };
+            };
+    });
 
-                var a = $(this).siblings(".quiz_answer_radio").val();
-                console.log(a);
+    document.addEventListener("click", function(event) {
+        
+        if (event.target.closest(".quiz_step") || event.target.closest(".quiz_result") || event.target.closest(".quiz_answer_box")) return;
+        step1.classList.remove("hidden");
+        step2.classList.remove("active");
+        answerO.classList.remove("active");
+        answerX.classList.remove("active");
+    });
+  
+    $(document).ready(function(){
+        //javascript
+      /*  var quiz_check = document.querySelectorAll('.quiz_answer_list_txt');
+        for(var i =0; i < quiz_check.length; i++) {
+            quiz_check[i].onclick = function(){
+                var a = this.previousElementSibling.value,         
+                    step1 = document.querySelector(".quiz_step_img"),
+                    step2 = document.querySelector(".quiz_step_txt"),
+                    answerO = document.querySelector(".quiz_result_O"),
+                    answerX = document.querySelector(".quiz_result_X");
+                console.log(this.previousElementSibling.value);
+
+                if(a == "1" || "2" || "3" || "4"){
+                    step1.classList.remove("hidden");
+                    step2.classList.remove("active");
+                    answerO.classList.remove("active");
+                    answerX.classList.remove("active");
+                };
+
+                var result = document.querySelector(".quiz_step_img");
+                result.onclick = function(){
+                    if(q_answer[0] == a){
+                        answerO.classList.add("active");
+                        answerX.classList.remove("active");
+                    }else{
+                        answerO.classList.remove("active");
+                        answerX.classList.add("active");
+                    }
                 
-                var step1 = document.querySelector(".quiz_step_img");    
-                var step2 = document.querySelector(".quiz_step_txt");
-                var answerO = document.querySelector(".quiz_result_O");
-                var answerX = document.querySelector(".quiz_result_X");
+                step1.classList.add("hidden");
+                step2.classList.add("active");
+                };
+            };
+        };*/
+
+        // javascript & jquery 4 (코드 최적화)
+        /*$(".quiz_answer_list_txt").click(function(){
+                var a = $(this).siblings(".quiz_answer_radio").val(),                
+                    step1 = document.querySelector(".quiz_step_img"),
+                    step2 = document.querySelector(".quiz_step_txt"),
+                    answerO = document.querySelector(".quiz_result_O"),
+                    answerX = document.querySelector(".quiz_result_X");
+                //console.log(a);
 
                 if(a == "1" || "2" || "3" || "4"){
                     step1.classList.remove("hidden");
@@ -23,9 +94,6 @@ var q_answer = ["2"];
                 }
                     var result = document.querySelector(".quiz_step_img");
                     result.onclick = function(){
-                    
-                        
-
                         if(q_answer[0] == a){
                             answerO.classList.add("active");
                             answerX.classList.remove("active");
@@ -38,10 +106,12 @@ var q_answer = ["2"];
                     step2.classList.add("active");
                     }
             
-        });
+        });*/
 
         // javascript & jquery 3
-       /* for(var i =0; i < el.length; i++) {
+       /* \        
+       var el = document.querySelectorAll('.quiz_answer_list_txt');
+        for(var i =0; i < el.length; i++) {
             el[i].onclick = function() { 
                 //alert();
                 var a = $(this).siblings(".quiz_answer_radio").val();
@@ -124,7 +194,7 @@ var q_answer = ["2"];
     /*    }
         step1();*/
 
-        var step1 = document.querySelector(".quiz_step_img");    
+       /* var step1 = document.querySelector(".quiz_step_img");    
         var step2 = document.querySelector(".quiz_step_txt");
         var answerO = document.querySelector(".quiz_result_O");
         var answerX = document.querySelector(".quiz_result_X");
@@ -140,7 +210,7 @@ var q_answer = ["2"];
             //$(".quiz_answer_radio:checkbox[name='quiz_answer']").css("outline","2px solid red").removeAttr("checked");
             //$(".quiz_answer_radio").siblings(".quiz_answer_list_txt").css("outline","2px solid red").prop("checked", false);            
             //console.log(e);
-        });
+        });*/
 
    });
        
