d1de137c721e220ebb4dc3ada1dbe153db5aaa52
songhee <kshs24820@gmail.com>
2019-09-17 16:11:52 +0900

    code optimization in js
    modify html, css, js

--- quiz/js/quiz.js
+++ quiz/js/quiz.js
@@ -1,13 +1,15 @@
-    //javascript 
+      
     document.addEventListener("DOMContentLoaded", function() { 
+       
+        step1 = document.querySelector(".quiz_step_img"),
+        step2 = document.querySelector(".quiz_step_txt"),
+        answerO = document.querySelector(".quiz_result_O"),
+        answerX = document.querySelector(".quiz_result_X");
+
         var quiz_check = document.querySelectorAll('.quiz_answer_list_txt');
             for(var i =0; i < quiz_check.length; i++) {
                 quiz_check[i].onclick = function(){
-                    var a = this.previousElementSibling.value;         
-                        step1 = document.querySelector(".quiz_step_img"),
-                        step2 = document.querySelector(".quiz_step_txt"),
-                        answerO = document.querySelector(".quiz_result_O"),
-                        answerX = document.querySelector(".quiz_result_X");
+                    var a = this.previousElementSibling.value;
 
                     if(a == "1" || "2" || "3" || "4"){
                         step1.classList.remove("hidden");
@@ -32,16 +34,17 @@
                 };
             };
     });
-
-    document.addEventListener("click", function(event) {
+    
+     document.addEventListener("click", function(event) {
         
-        if (event.target.closest(".quiz_step") || event.target.closest(".quiz_result") || event.target.closest(".quiz_answer_box")) return;
+        if (event.target.closest(".quiz_step") || event.target.closest(".quiz_result") || event.target.closest(".quiz_answer_list")) return;
+
         step1.classList.remove("hidden");
         step2.classList.remove("active");
         answerO.classList.remove("active");
         answerX.classList.remove("active");
     });
-  
+
     $(document).ready(function(){
         //javascript
       /*  var quiz_check = document.querySelectorAll('.quiz_answer_list_txt');
@@ -53,14 +56,12 @@
                     answerO = document.querySelector(".quiz_result_O"),
                     answerX = document.querySelector(".quiz_result_X");
                 console.log(this.previousElementSibling.value);
-
                 if(a == "1" || "2" || "3" || "4"){
                     step1.classList.remove("hidden");
                     step2.classList.remove("active");
                     answerO.classList.remove("active");
                     answerX.classList.remove("active");
                 };
-
                 var result = document.querySelector(".quiz_step_img");
                 result.onclick = function(){
                     if(q_answer[0] == a){
@@ -85,7 +86,6 @@
                     answerO = document.querySelector(".quiz_result_O"),
                     answerX = document.querySelector(".quiz_result_X");
                 //console.log(a);
-
                 if(a == "1" || "2" || "3" || "4"){
                     step1.classList.remove("hidden");
                     step2.classList.remove("active");
@@ -141,12 +141,10 @@
         /*for(var i =0; i < el.length; i++) {
             el[i].onclick = function() { 
                 var a = $(this).siblings(".quiz_answer_radio").val();
-
                 var step1 = document.querySelector(".quiz_step_img");    
                 var step2 = document.querySelector(".quiz_step_txt");
                 var answerO = document.querySelector(".quiz_result_O");
                 var answerX = document.querySelector(".quiz_result_X");
-
                 if(q_answer[0] == a){
                     answerO.classList.add("active");
                     answerX.classList.remove("active");
@@ -164,12 +162,10 @@
                 $(".quiz_answer_list_txt").click(function(){
             
                 var a = $(this).siblings(".quiz_answer_radio").val();
-
                 var step1 = document.querySelector(".quiz_step_img");    
                 var step2 = document.querySelector(".quiz_step_txt");
                 var answerO = document.querySelector(".quiz_result_O");
                 var answerX = document.querySelector(".quiz_result_X");
-
                 /*var a_check = document.querySelector('input[name="quiz_answer"]:checked');
                 console.log(a_check);*/
 
@@ -198,11 +194,9 @@
         var step2 = document.querySelector(".quiz_step_txt");
         var answerO = document.querySelector(".quiz_result_O");
         var answerX = document.querySelector(".quiz_result_X");
-
         document.addEventListener("click", function(event) {
             
             if (event.target.closest(".quiz_step") || event.target.closest(".quiz_result") || event.target.closest(".quiz_answer_box")) return;
-
             step1.classList.remove("hidden");
             step2.classList.remove("active");
             answerO.classList.remove("active");
@@ -213,5 +207,5 @@
         });*/
 
    });
-       
+
     
